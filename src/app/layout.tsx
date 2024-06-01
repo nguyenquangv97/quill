ee6b5527942e6ed887dc8d26eee8b60373f97055
Quang Nguyen <nguyenquangv97@gmail.com>
2024-06-01 16:33:08 -0700

    [Add] Suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
-
+import { Suspense } from 'react';
 import './globals.css';
 import { cn } from '@/lib/utils';
 const inter = Inter({ subsets: ['latin'] });
@@ -23,21 +23,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="light">
-      {/* <Suspense> */}
-      <Providers>
-        <body
-          className={cn(
-            'min-h-screen font-sans antialiased grainy',
-            inter.className
-          )}
-        >
-          <Toaster />
+      <Suspense>
+        <Providers>
+          <body
+            className={cn(
+              'min-h-screen font-sans antialiased grainy',
+              inter.className
+            )}
+          >
+            <Toaster />
 
-          <Navbar />
-          {children}
-        </body>
-      </Providers>
-      {/* </Suspense> */}
+            <Navbar />
+            {children}
+          </body>
+        </Providers>
+      </Suspense>
     </html>
   );
 }
